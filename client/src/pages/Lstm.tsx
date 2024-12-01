f07fc55f373fa61d9f68d65f8d1cf38f47d97061
Krishna Kanta Bera <126865352+krishnaKanta2008@users.noreply.github.com>
2024-12-01 18:01:46 +0530

    Update Lstm.tsx

--- client/src/pages/Lstm.tsx
+++ client/src/pages/Lstm.tsx
@@ -1,7 +1,7 @@
 import { AppSidebar } from "@/components/Sidebar/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { SidebarInset } from "@/components/ui/sidebar";
-import LSTMPage from "@/components/Prediction/BILSTM/BiLSTMPage";
+import LSTMPage from "@/components/Prediction/LSTM/LSTMPage";
 import AppHeader from "@/components/Sidebar/app-header";
 import { useCallback, useState, useEffect } from "react";
 import { fetchStockData } from "@/lib/actions";
